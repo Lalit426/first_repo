20117510d250196566795fba3d41ffb6e1187067
Lalit426 <luckyjoshi6894@gmail.com>
2026-06-08 04:54:27 -0700

    Add files via upload

---
+++ area_of_square.py
@@ -0,0 +1,53 @@
+#a=int(input("Enter the  side:"))
+#result=a*a
+#print(f"the area of  square is={a*a}")
+
+
+#Area of triangle=1/2*base*height
+""" base=float(input("Enter the base:"))
+height=float(input("Enter the hieght:"))
+answer=(1/2*base*height)
+print(f"the area of  triangle={answer}")"""
+
+#Area of circle=3.14*radius*radius
+"""r=int(input("Enter the radious:"))
+PI=3.14
+result=PI*r*r
+print(f"The radious of circle is:{result}")"""
+
+
+#Volume of sphere=4/3*3.14*radius*radius*radius
+"""r=int(input("Enter the radius:"))
+PI=3.14
+B=4/3*PI*r**3
+print(f"volum os sphere is {B}")"""
+
+
+#Perimeter of Rectangle = 2 * (length + width)
+"""length=int(input("Enter the length: "))
+width=int(input("Enter the width Rectangle: "))
+result=2*(length+width)
+print(f"the perameter of:{result}")"""
+
+
+#Simple Interest = (Principal * Rate * Time) / 100
+"""principal=int(input("Enter principal: "))
+Rate=int(input("Enter the Rate: "))
+Time=int(input("enter the Time: "))
+answer=(principal*Rate*Time)/100
+print(f"Simple Interest:{answer} ")"""
+
+#Celsius to Fahrenheit = (Celsius * 9/5) + 32
+"""celsius=int(input("Enter the celsius: "))
+result=(celsius*9/5)+32
+print(result)"""
+
+#Fahrenheit to Celsius = (Fahrenheit - 32) * 5/9
+"""Fahrenheit=int(input("Enter the celsius: "))
+result= (Fahrenheit-32)*5/9
+print(result)"""
+
+#Surface Area of Cube = 6 * side * side
+"""side=int(input("Enter the side:"))
+result = (side*side)*6
+print(result)"""
